84eb618882ed1d768976f8273915d3625321119a
Federico Builes <federico.builes@gmail.com>
2008-12-02 12:15:21 -0500

    Copy all the required files on install.

--- install.rb
+++ install.rb
@@ -1,8 +1,20 @@
 require 'fileutils'
 css_path = File.join(RAILS_ROOT, "public", "stylesheets", "calendar")
+js_path  = File.join(RAILS_ROOT, "public", "javascripts", "calendar")
 img_path = File.join(RAILS_ROOT, "public", "images", "calendar")
+
+ assets_path = File.join(File.dirname(__FILE__), "lib", "assets")
 
 FileUtils.mkdir_p css_path
 FileUtils.mkdir_p img_path
+FileUtils.mkdir_p js_path
 
-FileUtils.cp 
+FileUtils.cp(File.join(assets_path, "javascripts", "facebox.js"), js_path)
+
+%w(red.css blue.css grey.css facebox.css).each do |stylesheet|
+  FileUtils.cp(File.join(assets_path, "stylesheets", stylesheet), css_path)
+end
+
+Dir[assets_path + "/images/*"].each do |image|
+  FileUtils.cp(image, img_path)
+end
