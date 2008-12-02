9cfa64a447593c2771fbfd10e34194a837e99e20
Federico Builes <federico.builes@gmail.com>
2008-12-02 12:03:22 -0500

    Adding an install.rb script.

--- install.rb
+++ install.rb
@@ -1,1 +1,8 @@
-# Install hook code here
+require 'fileutils'
+css_path = File.join(RAILS_ROOT, "public", "stylesheets", "calendar")
+img_path = File.join(RAILS_ROOT, "public", "images", "calendar")
+
+FileUtils.mkdir_p css_path
+FileUtils.mkdir_p img_path
+
+FileUtils.cp 
