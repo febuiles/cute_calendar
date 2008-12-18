130dbb7db8a8ff12d6ccb30389ae0f03ff56418d
Federico Builes <federico.builes@gmail.com>
2008-12-18 11:23:03 -0500

    Updating install.rb to copy the generator.

--- install.rb
+++ install.rb
@@ -1,31 +1,31 @@
 require 'fileutils'
 
-# copy assets
 css_path = File.join(RAILS_ROOT, "public", "stylesheets", "calendar")
 js_path  = File.join(RAILS_ROOT, "public", "javascripts", "calendar")
 img_path = File.join(RAILS_ROOT, "public", "images", "calendar")
 partials_path = File.join(RAILS_ROOT, "app", "views", "shared")
+generators_path = File.join(RAILS_ROOT, "lib", "generators")
 
 assets_path = File.join(File.dirname(__FILE__), "lib", "assets")
+templates_path = File.join(File.dirname(__FILE__), "generators")
 
+# create all the required directories if they don't exist
 FileUtils.mkdir_p css_path
 FileUtils.mkdir_p img_path
 FileUtils.mkdir_p js_path
 FileUtils.mkdir_p partials_path
+FileUtils.mkdir_p generators_path
 
+# copy all the assets
 FileUtils.cp(File.join(assets_path, "javascripts", "jquery.js"), js_path)
 FileUtils.cp(File.join(assets_path, "javascripts", "facebox.js"), js_path)
-
 FileUtils.cp(File.join(assets_path, "partials", "_calendar.html.erb"), partials_path)
-
 %w(red.css blue.css grey.css facebox.css).each do |stylesheet|
   FileUtils.cp(File.join(assets_path, "stylesheets", stylesheet), css_path)
 end
-
 Dir[assets_path + "/images/*"].each do |image|
   FileUtils.cp(image, img_path)
 end
 
-# create a new Event model with its migration
-# TODO: This should use a generator or something
-`cd #{RAILS_ROOT} && ruby script/generate model -s --skip-fixture Event title:string description:text date:date`
+# copy the generator
+FileUtils.cp(File.join(templates_path, "event_generator.rb"), generators_path)
