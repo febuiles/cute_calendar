d2e01dba1eca612c95563d4a177fc8427030d9a3
Federico Builes <federico.builes@gmail.com>
2008-12-18 12:24:51 -0500

    Shorter queries.

--- generators/events/templates/models/event.rb
+++ generators/events/templates/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
   # checks if an event exists on a given date
   def self.special_date?(date)
-    events ||= self.all
+    events ||= self.all(:select => "id, date")
     events.select{ |ev| ev.date == date}.size > 0
   end
 
