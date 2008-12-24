cd0615ab2135e631a9ed19db69223b118b58ce98
Federico Builes <federico.builes@gmail.com>
2008-12-23 21:03:25 -0500

    Adding an uninstall.rb script.

--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,12 @@
-# Uninstall hook code here
+require 'fileutils'
+
+css_path = File.join(RAILS_ROOT, "public", "stylesheets", "calendar")
+js_path  = File.join(RAILS_ROOT, "public", "javascripts", "calendar")
+img_path = File.join(RAILS_ROOT, "public", "images", "calendar")
+generators_path = File.join(RAILS_ROOT, "lib", "generators")
+
+FileUtils.rm_rf css_path
+FileUtils.rm_rf js_path
+FileUtils.rm_rf img_path
+FileUtils.rm(File.join(generators_path, "events_generator.rb"))
+
